7e47da76183b986ce637188b4d57446dd6cc4156
aryangupta701 <garyan447@gmail.com>
2022-06-22 01:09:38 +0530

    added css in productDetails

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -36,7 +36,7 @@ export const getProductDetails= (id) => async(dispatch) => {
             type : PRODUCT_DETAILS_REQUEST
         })
         const {data} = await axios.get(`/api/v1/product/${id}`);
-        console.log(data)
+        // console.log(data)
         dispatch({
             type: PRODUCT_DETAILS_SUCCESS,
             payload : data
--- src/components/Product/ProductDetails.js
+++ src/components/Product/ProductDetails.js
@@ -4,33 +4,81 @@ import {useSelector, useDispatch} from 'react-redux'
 import './ProductDetails.css'
 import {getProductDetails} from '../../actions/productActions'
 import { useParams } from 'react-router-dom'
+import ReactStars from 'react-rating-stars-component'
 
 const ProductDetails = ({match}) => {
     const dispatch = useDispatch()
     const {id} = useParams()
-    const {product} = useSelector(state => state.productDetails)
+    const {product,loading,error} = useSelector(state => state.productDetails)
     useEffect(()=>{
         dispatch(getProductDetails(id))
     },[dispatch,id])
-    console.log(id)
+    // console.log(id)
+    const stars = {
+        edit : false, 
+        color : "rgba(20,20,20,0,1)",
+        activeColor: "tomato",
+        value:product && product.ratings,
+        isHalf: true,
+        size : window.innerWidth < 600 ? 20 : 25
+    }
     return (
+     <Fragment>
+        {product && 
         <Fragment>
             <div className='productDetails'>
                 <div>
                     <Carousel>
                         {product && product.images &&
                         product.images.map((item,i)=>{
-                            <img 
+                            return <img 
                             className='carousel-img'
-                            key = {item.url}
-                            src = {item.url}
+                            key = {item.URL}
+                            src = {item.URL}
                             alt = {`${i} image`} />
                         })
                         }   
                     </Carousel>
                 </div>
+                <div>
+                    <div className='details-block-one'>
+                        <h2>{product.name}</h2>
+                        <p>Product #{product._id}</p>
+                    </div>
+                    <div className='details-block-two'>
+                        <ReactStars {...stars} />
+                        <span className="detailsBlock-2-span">
+                            {" "}
+                            ({product.numOfReviews} Reviews)
+                        </span>
+                    </div>
+                    <div className='details-block-three'>
+                        <h1>{`₹${product.price}`}</h1>
+                        <div className='details-block-three-one'>
+                            <div className='details-block-three-one-one'>
+                                <button>-</button>
+                                <input value="1" type="number"/>
+                                <button>+</button>
+                            </div>
+                            {" "}<button className='add-to-cart-button'>Add to Cart</button>
+                            <p>
+                                Status:{" "}
+                                <b className={product.stock < 1 ? 'redColor' : 'greenColor'}>
+                                    {product.stock<1 ? "OutOfStock" : "InStock"}
+                                </b>
+                            </p>
+                        </div>
+                        <div className='details-block-four'>
+                            Description : <p>{product.description}</p>
+                        </div>
+                        <button className='submit-button'>Submit Review</button>
+                    </div>
+                </div>
             </div>
         </Fragment>
+        }
+    </Fragment>
+                    
   )
 }
 
